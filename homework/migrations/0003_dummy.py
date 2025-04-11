d709de5458c4a6f937a1a1935d83dd9ff5b11c6b
queen <kafikoa77@gmail.com>
2025-04-11 13:41:24 +0100

    Render-safe: drop broken tables before real migrations

---
+++ homework/migrations/0003_dummy.py
@@ -0,0 +1,12 @@
+# homework/migrations/0003_dummy.py
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('homework', '0002_initial'),
+    ]
+
+    operations = [
+        # No operations
+    ]
