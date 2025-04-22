9ad79c0969898e6766077c473730aba7e7691e9b
queen <kafikoa77@gmail.com>
2025-04-22 15:38:30 +0100

    altered .env

--- init_setup.py
+++ init_setup.py
@@ -12,12 +12,12 @@ print("Migrations applied successfully.")
 
 # Create superuser if it doesn't exist
 User = get_user_model()
-if not User.objects.filter(username=os.getenv("DJANGO_SUPERUSER_USERNAME")).exists():
-    User.objects.create_superuser(
-        username=os.getenv("DJANGO_SUPERUSER_USERNAME"),
-        email=os.getenv("DJANGO_SUPERUSER_EMAIL"),
-        password=os.getenv("DJANGO_SUPERUSER_PASSWORD"),
-    )
-    print("Superuser created successfully.")
-else:
-    print("Superuser already exists. Skipping creation.")
+#if not User.objects.filter(username=os.getenv("DJANGO_SUPERUSER_USERNAME")).exists():
+User.objects.create_superuser(
+    username=os.getenv("DJANGO_SUPERUSER_USERNAME"),
+    email=os.getenv("DJANGO_SUPERUSER_EMAIL"),
+    password=os.getenv("DJANGO_SUPERUSER_PASSWORD"),
+)
+print("Superuser created successfully.")
+#else:
+#    print("Superuser already exists. Skipping creation.")
